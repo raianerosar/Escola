1affcfde7a73f7a8f72c8e1590946646c6dd951b
raianerosar <nanirosar@gmail.com>
2026-04-15 11:24:24 -0300

    chore: exclude .worktrees/ from jest test paths

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.worktrees/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
